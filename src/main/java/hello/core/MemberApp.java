b6d12386e66ceea5aee75cd813ff9d6203f75886
maeng-book <dakma@naver.com>
2025-05-27 08:02:46 +0900

    섹션 4. 스프링 핵심 원리 이해2 - 스프링으로 전환하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
 
-        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        // AppConfig 버전 -> Spring 버전 변경
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "member A", Grade.VIP);
         System.out.println("New Member = " + member.getName());
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -4,15 +4,22 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        // AppConfig 버전 -> Spring 버전 변경
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member createVIPMember = new Member(1L, "VIP Member", Grade.VIP);
         memberService.join(createVIPMember);
@@ -23,7 +30,7 @@ public class OrderApp {
         Member findVIPMember = memberService.findMember(createVIPMember.getId());
         Member findBasicMember = memberService.findMember(createBasicMember.getId());
 
-        Order vipOrder = orderService.createOrder(findVIPMember.getId(), "VIP Item", 5000);
+        Order vipOrder = orderService.createOrder(findVIPMember.getId(), "VIP Item", 20000);
         System.out.println("vipOrder = " + vipOrder);
         System.out.println("vipOrder.calculatePrice() = " + vipOrder.calculatePrice());
 
